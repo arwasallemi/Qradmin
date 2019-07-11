72b7decced897d5383c05ab6b10928b661b81e0b
nahla2017 <nahlabaccar94@gmail.com>
2019-07-11 11:05:16 +0100

    imag

--- src/pages/image-qrcode/image-qrcode.ts
+++ src/pages/image-qrcode/image-qrcode.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProduitProvider } from '../../providers/produit/produit';
 import { Printer, PrintOptions } from '@ionic-native/printer';
+import { SocieteProvider } from '../../providers/societe/societe';
 
 /**
  * Generated class for the ImageQrcodePage page.
@@ -18,13 +19,29 @@ import { Printer, PrintOptions } from '@ionic-native/printer';
 export class ImageQrcodePage {
   imageQrList:any=[]
   item 
+  imagelist
   nombre: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams , public providerproduit : ProduitProvider,private printer: Printer) {
-    
+  societeprovider: any;
+  list: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams , public providerproduit : ProduitProvider,private printer: Printer,
+    private societe:SocieteProvider) {
+    this.loadImage()
     this.item=this.navParams.get('item');
     this.nombre=this.navParams.get('nombre');
     console.log('hhhhhhhhhhh:',this.nombre);
-    this.imageQrList.length=this.nombre
+    //this.imageQrList.length=(this.nombre)/2
+    if((parseInt(this.nombre) % 2)==0){
+      //let nb =parseInt(this.nombre)
+      this.imageQrList.length=parseInt(this.nombre)/2
+      console.log('taille:', this.imageQrList.length);
+    }
+    
+    if((parseInt(this.nombre) % 2)==1)
+    { let nb =parseInt(this.nombre) + 1
+      this.imageQrList.length=(nb)/4
+      console.log('taille:', this.imageQrList.length);
+    }
+  
   }
 
   ionViewDidLoad() {
@@ -35,6 +52,30 @@ export class ImageQrcodePage {
     this.printer.isAvailable().then(this.onSuccessLoad, this.onErrorLoad);
 
   }
+  // get(){
+  //   this.societe.get()
+  //   .then(data => {
+  //     this.list = data;
+  
+  //  this.image=this.list[0].alt;
+  //  console.log("image:::::",this.image);
+ 
+  //   });
+  
+  // }
+  
+  loadImage(){
+
+    this.societe.loadimageSociete()
+    .then(data => {
+      this.imagelist = data;
+      console.log(this.imagelist)
+  
+  
+    });
+  
+  }
+
 
   onSuccessLoad(){
     let options: PrintOptions = {
